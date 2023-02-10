95a5ddf719a90b054d8e8f5c286ec49960bf883f
khushalmeh <khushalmehta96@gmail.com>
2023-02-10 20:29:53 +0530

    Done

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Layout from './../components/Layout/Layout';
 import  '../styles/formg.css'
 function ContactUs() {
+    const submitHandler=()=>{
+        alert("Ordered Placed Successfully")
+    }
   return (
     <Layout>
     <div className='k'>
-        <h1 className='j'><b>Order Now</b></h1>
-    <form className='group'>
+    <h1 className='j'><b>Order Now</b></h1>
+    <form className='group' onSubmit={submitHandler}>
    <label>Name :</label>
    <input type = "text" placeholder='Enter your Name' required='Required' ></input><br></br><br></br>
    <label>City :</label>
@@ -16,16 +19,6 @@ function ContactUs() {
    <label>Select Dishes :</label>
    <input type = "text" placeholder='Select Dishes' required='Required'></input><br></br><br></br>
    <button type='submit' className='l'>Submit</button>
-
-
-
-
-
-
-
-
-
-
     </form>
     </div>
     </Layout>
