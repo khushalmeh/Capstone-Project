3884b255caabdbd641df31b2b1b5afe9daef77a4
khushalmeh <khushalmehta96@gmail.com>
2023-03-23 21:58:15 +0530

    kjh

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -11,10 +11,10 @@ const Footer = () => {
    <>
   <Box sx={{bgcolor:"#1A1A19", color:"white", textAlign:"center", p:3}}>
     <Box sx={{my :2, "& svg ": {fontSize : "60px" , cursor : "pointer",mr: 2} , "& svg:hover" :{color:"goldenrod",transform:"translateX(5px)",transition : "all 400ms"}, }}>
-    <a href='https://www.instagram.com/khushal___mehta/' target='blank'><InstagramIcon /></a>
-    <a href='https://www.facebook.com/khushal.mehta.374' target='blank'><FacebookIcon/></a>
-    <a href='https://github.com/khushalmeh' target='blank'><GitHubIcon/></a>
-    <a href='https://www.linkedin.com/feed/' target='blank'><LinkedInIcon/></a>
+    <a href='https://www.instagram.com/_swastik_gupta/' target='blank'><InstagramIcon /></a>
+    <a href='https://www.instagram.com/_swastik_gupta/' target='blank'><FacebookIcon/></a>
+    <a href='https://github.com/SwastikGupta123' target='blank'><GitHubIcon/></a>
+    <a href='https://www.linkedin.com/in/swastik-gupta-1465441b6' target='blank'><LinkedInIcon/></a>
     </Box>
     <Typography variant='h5' sx ={{"@media (max-width:600px)": {fontSize : "1rem"} }}>
         All Right Reserved &copy; Restaurant
